fdcbbc8be6b3937a122b2d180133b7a00f919f43
farahnizam <48623543+farahnizam@users.noreply.github.com>
2024-03-14 15:03:39 +0500

    Test JS Script

---
+++ TestScript.js
@@ -0,0 +1,12 @@
+<script id="intelli-chat-widget-code">
+              (function (d, t, id) {
+                if (d.getElementById(id)) return;
+                var fjs = d.getElementsByTagName(t)[0];
+                var js = d.createElement(t);
+           
+                js.id = id;
+                js.src = 'https://wemsol.contegris.com:8443/widget/0c2e53f9bba6e1ed8af6f90dc788dc6b/embed.js';
+                js.async = true;
+                fjs.parentNode.insertBefore(js, fjs);
+              })(document, 'script', 'intelli-chat-widget');
+</script>
